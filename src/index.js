b586c3750f0b2437d1d2ffbce0da8f23a8f59bd8
rabinzon <ildarrazin@gmail.com>
2017-12-05 08:48:20 +0300

    feat: add loader logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-export default () => 'go!';
+import axios from 'axios';
+import fs from 'mz/fs';
+import path from 'path';
+
+const getPage = async (url) => {
+  const { data } = await axios.get(url);
+  return data;
+};
+
+const getFileName = (url) => {
+  const fileName = url.replace(/^.+:\/\//, '').replace(/\W/gi, '-');
+  return `${fileName}.html`;
+};
+
+export default async (url, outputPath) => {
+  if (!await fs.exists(outputPath)) {
+    console.log(`${outputPath} directory does not exist`);
+    return;
+  }
+
+  const fileName = getFileName(url);
+  const currentDir = path.resolve('./', fileName);
+  const outputDir = outputPath ? path.resolve(outputPath, fileName) : currentDir;
+  const page = await getPage(url);
+  console.log(`✔ ${url}\n`);
+  await fs.writeFile(outputDir, page);
+  console.log(`Page was downloaded as '${fileName}'`);
+};
